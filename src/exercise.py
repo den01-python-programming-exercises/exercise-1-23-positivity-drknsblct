97397b59d01f64c71c265e5776592876fdf89043
drknsblct <57183092+drknsblct@users.noreply.github.com>
2021-09-17 04:18:30 +0300

    Update exercise.py

--- src/exercise.py
+++ src/exercise.py
@@ -1,5 +1,11 @@
 def main():
     #write your code below this line
+    num = int(input('Give a number:'))
+    
+    if num >= 0:
+        print('Number is positive')
+    else:
+        print('Number is negative')
 
 if __name__ == '__main__':
     main()
